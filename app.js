ddd5f253c21f442dc8df68e530169ff7b71ee6ea
Jane Philipps <jane.philipps@gmail.com>
2015-05-31 12:00:12 -0700

    Add form to index

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ angular.module('spotPaintingApp', ['ngRoute'])
 
   .controller("MainCtrl", ['$scope', function ($scope) {
 
-    $scope.test = "it works!";
+    $scope.master = {};
 
-  }])
+    $scope.update = function (painting) {
+      $scope.master = angular.copy(painting);
+    };
+
+    $scope.reset = function () {
+      $scope.painting = angular.copy($scope.master);
+    };
+
+    $scope.reset();
+
+  }]);
