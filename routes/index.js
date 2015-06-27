a24c4b71de53dcf5643b1bbb805ff9517003be40
Jane Philipps <jane.philipps@gmail.com>
2015-06-27 13:56:19 -0700

    remove unnecessary code

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,8 @@ var Painting = require('../models/painting');
 
   module.exports = function (app) {
 
-    // server routes
-    // handle things like api calls
-    // authentication routes
-
-    // sample api route
-    // app.get('/api/paintings', function (req, res) {
-    //   // use mongoose to get all paintings in the database
-    //   Painting.find(function(err, paintings) {
-
-    //     // if there is an error retrieving, send the error.
-    //     // nothing after res.send(err) will execute
-
-    //     if (err) {
-    //       res.send(err);
-    //     }
-    //     res.json(paintings); // return all paintings in JSON format
-
-    //   });
-    // });
-
-
     // Route to save painting to DB
     app.post('/api/paintings', function (req, res) {
-      // console.log(req.body);
       Painting.create(req.body, function (err, painting) {
         console.log(painting);
         res.json(painting._id);
@@ -35,7 +13,6 @@ var Painting = require('../models/painting');
 
     // Route to gallery
     app.get('/api/paintings', function (req, res) {
-      console.log('I made it!');
       Painting.find()
       .then(function (paintings) {
         // Paintings are saved in DB weirdly, so we need to do this to fix them and make accessible
@@ -44,14 +21,12 @@ var Painting = require('../models/painting');
             arr[id] = colorStr.split(",");
           });
         });
-        console.log(paintings);
         res.json(paintings);
       })
     });
 
     // Route to painting
     app.get('/api/paintings/:id', function (req, res) {
-      console.log('painting!');
       Painting.find( { _id: req.params.id })
         .then(function (painting) {
           painting[0].painting.forEach(function(colorStr,id,arr){
@@ -64,6 +39,6 @@ var Painting = require('../models/painting');
     // route to handle creating goes here (app.post)
     // route to handle all angular requests
     app.get('*', function (req, res) {
-      res.sendfile('./public/views/index.html');
+      res.sendFile('./public/views/index.html');
     });
   };
