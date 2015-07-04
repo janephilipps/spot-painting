bd01f4064e86d9bd5d740d43a1b6d914cee8f170
Jane Philipps <jane.philipps@gmail.com>
2015-07-04 16:18:48 -0700

    make tweet button work

--- public/js/app.js
+++ public/js/app.js
@@ -316,13 +316,28 @@ angular.module('spotPaintingApp', ['ngRoute'])
 
   .controller("PaintingCtrl", ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
     console.log($routeParams);
-      $http.get('/api/paintings/' + $routeParams.id)
-        .success(function (painting) {
-          console.log(painting);
-          $scope.painting = painting;
-        })
-        .error(function (painting) {
-        })
+
+    // Tweet button script
+    var tweet = function(d,s,id){
+      var js,
+          fjs = d.getElementsByTagName(s)[0],
+          p = /^http:/.test(d.location)?'http':'https';
+      console.log("tweet");
+      console.log(d.location);
+      js = d.createElement(s);
+      js.id = id;
+      js.src = p + '://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js,fjs);
+    };
+
+    $http.get('/api/paintings/' + $routeParams.id)
+      .success(function (painting) {
+        console.log(painting);
+        tweet(document, 'script', 'twitter-wjs');
+        $scope.painting = painting;
+      })
+      .error(function (painting) {
+      })
 
   }])
 
