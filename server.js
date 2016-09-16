7648889ec3972daf5d73a4d860a71cdecdafd151
Jane Philipps <jane.philipps@gmail.com>
2016-09-15 22:09:44 -0700

    add secure cookies

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
-var env = process.env;
 var favicon = require('serve-favicon');
 var enforce = require('express-sslify');
 var passport = require('passport');
+var isProduction = process.env.NODE_ENV === 'production';
 
-if (env.MONGOLAB_URI) {
-  console.log("Connecting to prod mongo.");
-  mongoose.connect(env.MONGOLAB_URI);
+if (isProduction) {
+  console.log('Connecting to prod mongo.');
+  if (!process.env.MONGOLAB_URI) {
+    throw new Error('MONGOLAB_URI must be set.');
+  }
+  mongoose.connect(process.env.MONGOLAB_URI);
 } else {
-  console.log("Connecting to dev mongo");
+  console.log('Connecting to dev mongo');
   mongoose.connect('mongodb://localhost:27017/spot_painting_db');
 }
 
@@ -21,8 +24,10 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// Force HTTPS
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (isProduction) {
+  // Force HTTPS
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 
 // Set port
 var port = process.env.PORT || 3000;
@@ -41,12 +46,27 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(session({
-  secret: "I love Jane",
+var sessionConfig = {
+  secret: 'development',
   resave: false,
   saveUninitialized: true,
-  name: "sessionId"
-}));
+  name: 'sessionId',
+  cookie: {
+    maxAge: 2 * 7 * 24 * 60 * 60 * 1000 // Two weeks
+  }
+};
+
+if (isProduction) {
+  if (!process.env.SPOT_PAINTING_SECRET) {
+    throw new Error('SPOT_PAINTING_SECRET must be set.');
+  }
+  sessionConfig.secret = process.env.SPOT_PAINTING_SECRET;
+  sessionConfig.cookie.secure = true;
+  sessionConfig.cookie.httpOnly = true;
+  sessionConfig.cookie.domain = 'www.spot-painting.com';
+}
+
+app.use(session(sessionConfig));
 
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
