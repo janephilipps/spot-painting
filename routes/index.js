0026b26dd0656bfcc1d85463e48d1bb3b0ab374f
Jane Philipps <jane.philipps@gmail.com>
2016-12-20 20:33:27 -0800

    add SSL handshake route

--- routes/index.js
+++ routes/index.js
@@ -82,6 +82,15 @@ require('../config/passport')(passport);
         });
     });
 
+    var handshakePath = process.env.HANDSHAKE_PATH;
+    var handshakeResponse = process.env.HANDSHAKE_RESPONSE;
+
+    if (handshakePath && handshakeResponse) {
+      app.get('/.well-known/acme-challenge/' + handshakePath, function (req, res) {
+        res.send(handshakeResponse);
+      });
+    }
+
     app.get('*', function (req, res) {
       res.sendfile('./public/views/index.html');
     });
