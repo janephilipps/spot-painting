b34303c318225992fe388f54dff6e19e1e838c89
Jane Philipps <jane.philipps@gmail.com>
2017-06-02 21:24:15 -0700

    add renderCanvases to CanvasService

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -1,4 +1,4 @@
-angular.module('ProfileCtrl', []).controller('ProfileController', ['AuthService', '$scope', '$http', '$location', '$routeParams', '$timeout', function(AuthService, $scope, $http, $location, $routeParams, $timeout) {
+angular.module('ProfileCtrl', []).controller('ProfileController', ['AuthService', 'CanvasService', '$scope', '$http', '$location', '$routeParams', '$timeout', function(AuthService, CanvasService, $scope, $http, $location, $routeParams, $timeout) {
 
   $scope.pageSize = Math.min(20, $location.search().pageSize) || 5;
 
@@ -59,7 +59,7 @@ angular.module('ProfileCtrl', []).controller('ProfileController', ['AuthService'
         $location.search('page', 1);
       }
       $timeout(function() {
-        $scope.renderCanvas();
+        CanvasService.renderCanvases($scope.paintings);
       });
     });
 
@@ -68,38 +68,5 @@ angular.module('ProfileCtrl', []).controller('ProfileController', ['AuthService'
            AuthService.isLoggedIn() &&
            ($scope.profileUser._id === AuthService.getLoggedInUser()._id);
   };
-
-  $scope.renderCanvas = function() {
-
-    var paintings = $scope.paintings;
-
-    var canvases = document.getElementsByClassName('painting');
-
-    for (var k = 0; k < paintings.length; k++) {
-      // debugger;
-      var canvas = canvases[k].children[0];
-      var ctx = canvas.getContext('2d');
-      ctx.canvas.width = (paintings[k].columns * 25) + ((paintings[k].columns - 1) * 20);
-      ctx.canvas.height = (paintings[k].rows * 25) + ((paintings[k].rows - 1) * 20);
-      var x = 12.5;
-      var y = 12.5;
-
-      ctx.fillStyle = '#FFF';
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-      for (var j = 0; j < paintings[k].painting.length; j++) {
-        for (var i = 0; i < paintings[k].painting[j].length; i++) {
-          ctx.fillStyle = paintings[k].painting[j][i];
-          ctx.beginPath();
-          ctx.arc(x, y, 12.5, 0, 2 * Math.PI);
-          ctx.fill();
-          x += 45;
-          ctx.moveTo(x, y);
-        }
-        x = 12.5;
-        y += 45;
-      }
-    }
-  }
 
 }]);
--- public/js/services/CanvasService.js
+++ public/js/services/CanvasService.js
@@ -1,9 +1,9 @@
-angular.module('Canvas', []).factory('CanvasService', ['$http', function($http) {
+angular.module('Canvas', []).factory('CanvasService', [function() {
 
   var service = {};
 
   service.renderCanvas = function(painting) {
-    console.log('CANVAS!!!');
+    console.log('CANVAS SERVICE!!!');
 
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
@@ -28,6 +28,38 @@ angular.module('Canvas', []).factory('CanvasService', ['$http', function($http) 
       y += 45;
     }
   };
+
+  service.renderCanvases = function(paintings) {
+
+    console.log('renderCanvases!!!');
+
+    var canvases = document.getElementsByClassName('painting');
+
+    for (var k = 0; k < paintings.length; k++) {
+      var canvas = canvases[k].children[0];
+      var ctx = canvas.getContext('2d');
+      ctx.canvas.width = (paintings[k].columns * 25) + ((paintings[k].columns - 1) * 20);
+      ctx.canvas.height = (paintings[k].rows * 25) + ((paintings[k].rows - 1) * 20);
+      var x = 12.5;
+      var y = 12.5;
+
+      ctx.fillStyle = '#FFF';
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+      for (var j = 0; j < paintings[k].painting.length; j++) {
+        for (var i = 0; i < paintings[k].painting[j].length; i++) {
+          ctx.fillStyle = paintings[k].painting[j][i];
+          ctx.beginPath();
+          ctx.arc(x, y, 12.5, 0, 2 * Math.PI);
+          ctx.fill();
+          x += 45;
+          ctx.moveTo(x, y);
+        }
+        x = 12.5;
+        y += 45;
+      }
+    }
+  }
 
   return service;
 
